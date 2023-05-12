574c13a1519326e58d5a1039d59cf6ce25659944
kathiravan <kathiravans@boodskap.io>
2023-05-12 06:59:02 +0530

    added observable using getter

--- src/app/getter-setter/getter-setter.component.ts
+++ src/app/getter-setter/getter-setter.component.ts
@@ -6,11 +6,16 @@ import { GetterSetterService } from './getter-setter.service';
   template: ''
 })
 export class GetterSetterComponent {
-  constructor(private getterSetterService: GetterSetterService) { }
+  constructor(private getterSetterService: GetterSetterService) {
+    this.getterSetterService.sample$.subscribe(value => {
+      console.log(value);
+    })
+   }
   
   ngOnInit() {
     console.log(this.getterSetterService.data);
     this.getterSetterService.data = "hi!"
     console.log(this.getterSetterService.data);
+    this.getterSetterService.sampleVal = "ge"
   }
 }
--- src/app/getter-setter/getter-setter.service.ts
+++ src/app/getter-setter/getter-setter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +7,9 @@ import { Injectable } from '@angular/core';
 export class GetterSetterService {
 
   private _data: string = "hello"
+
+  private sample = new BehaviorSubject<string>("gm")
+  sample$ = this.sample.asObservable()
   
   get data():string {
     return this._data
@@ -13,5 +17,8 @@ export class GetterSetterService {
 
   set data(value: string) {
     this._data = value
+  }
+  set sampleVal(value:string) {
+    this.sample.next(value)
   }
 }
